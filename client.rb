c9c513639bb271152c38aac64b287a8d2680d63e
Sebastián Morales <sebasuni96@gmail.com>
2023-08-28 20:48:19 -0300

    Fix: solo se trae la primera página

--- client.rb
+++ client.rb
@@ -4,36 +4,40 @@ require 'faraday'
 require 'json'
 require_relative 'clientdefaults'
 
-class SoloMira
-  include ClientDefaults
-  def initialize
-    @conn = connection
-  end
-
-  def search_for_item(query)
-    payload = {
-      query:
-    }
-
-    response = @conn.post do |req|
-      req.body = payload.to_json
+module SoloMira
+  class Client
+    include ClientDefaults
+    def initialize
+      @conn = connection
     end
 
-    puts response.body
-  end
+    def search_movie(query)
+      payload = {
+        query:,
+        content_type: 'movie',
+        page_size: PAGE_SIZE
+      }
 
-  private
+      response = @conn.post do |req|
+        req.body = payload.to_json
+      end
 
-  def connection
-    headers = {
-      'User-Agent' => 'JustWatch client sebamoralesasd'
-    }
-    Faraday.new(url: BASE_URL) do |faraday|
-      faraday.adapter Faraday.default_adapter
-      faraday.headers = headers
+      response.body
+    end
+
+    private
+
+    def connection
+      headers = {
+        'User-Agent' => 'JustWatch client github.com/sebamoralesasd/solomira'
+      }
+      Faraday.new(url: BASE_URL) do |faraday|
+        faraday.adapter Faraday.default_adapter
+        faraday.headers = headers
+      end
     end
   end
 end
 
-cli = SoloMira.new
-cli.search_for_item('Shrek')
+cli = SoloMira::Client.new
+puts cli.search_movie('Shrek')
--- clientdefaults.rb
+++ clientdefaults.rb
@@ -2,4 +2,5 @@
 
 module ClientDefaults
   BASE_URL = 'https://apis.justwatch.com/content/titles/es_AR/popular'
+  PAGE_SIZE = 1
 end
